bfa1cd18b4721a7b8ac0e60bc79fdcaa1e0392bc
Zeeshan <rumair4040@gmail.com>
2021-08-23 13:03:40 +0500

    Initial commit

---
+++ app/src/main/java/com/ashish/roomdemo/activities/MainActivity.java
@@ -0,0 +1,95 @@
+package com.ashish.roomdemo.activities;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+
+import com.ashish.roomdemo.R;
+import com.ashish.roomdemo.adaptors.PersonAdaptor;
+import com.ashish.roomdemo.database.AppDatabase;
+import com.ashish.roomdemo.database.AppExecutors;
+import com.ashish.roomdemo.model.Person;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton floatingActionButton;
+    private RecyclerView mRecyclerView;
+    private PersonAdaptor mAdapter;
+    private AppDatabase mDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.addFAB);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EditActivity.class));
+            }
+        });
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the adapter and attach it to the RecyclerView
+        mAdapter = new PersonAdaptor(this);
+        mRecyclerView.setAdapter(mAdapter);
+        mDb = AppDatabase.getInstance(getApplicationContext());
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            // Called when a user swipes left or right on a ViewHolder
+            @Override
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                // Here is where you'll implement swipe to delete
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int position = viewHolder.getAdapterPosition();
+                        List<Person> tasks = mAdapter.getTasks();
+                        mDb.personDao().delete(tasks.get(position));
+                        retrieveTasks();
+                    }
+                });
+            }
+        }).attachToRecyclerView(mRecyclerView);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        retrieveTasks();
+    }
+
+    private void retrieveTasks() {
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                final List<Person> persons = mDb.personDao().loadAllPersons();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter.setTasks(persons);
+                    }
+                });
+            }
+        });
+
+
+    }
+}
